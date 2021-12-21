4f3e3664c58f7c84d443439c91e8cad2bf6fa5c0
Prabakaran <prabakaran.k@nmbrsystems.com>
2021-12-21 22:16:34 +0530

    changes made for other pages also like ac-gas component

--- src/app/layout/services/repair-rent/repair-rent.component.ts
+++ src/app/layout/services/repair-rent/repair-rent.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepairRentComponent implements OnInit {
 
+  certificates = [
+    { bgColor: '#1e4875', title: 'Fully Client Satisfaction', content: 'We always giving fully satisfaction to our all valuable customers.', img: 'https://aircoolsolutions.in/img/icon/award-1.png' },
+    { bgColor: '#ca232a', title: 'Professional Workers', content: 'Indina Aircool is team of expert and experienced technicians.', img: 'https://aircoolsolutions.in/img/icon/award-2.png' },
+    { bgColor: '#176bc4', title: 'Service Guarantee', content: 'We always provide 90 days service warranty without any condition.', img: 'https://aircoolsolutions.in/img/icon/award-3.png' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
